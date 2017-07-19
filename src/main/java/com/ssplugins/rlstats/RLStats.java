3ebc9d92731864671a54ab49db86120621e43ee5
567legodude <oparkerj@gmail.com>
2017-07-18 17:01:00 -0700

    Fixed version string.

--- src/main/java/com/ssplugins/rlstats/RLStats.java
+++ src/main/java/com/ssplugins/rlstats/RLStats.java
@@ -17,7 +17,7 @@ public final class RLStats {
 	/**
 	 * Current version of the library.
 	 */
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.0-beta";
 	
 	/**
 	 * User agent used for making requests.
